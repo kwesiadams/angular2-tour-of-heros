a0d37b2e1a30bfcd8fa3b4028dbed55dc1132aa4
kwesiadams <kwesi.adams@gmail.com>
2016-03-29 16:54:23 -0700

    Step just before router

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,24 @@
 /**
  * Created by kwadams on 3/29/2016.
  */
-import {Component} from 'angular2/core';
-import {HeroesComponent} from "./heroes.component";
-import {HeroService} from "./hero.service";
+import { Component }       from 'angular2/core';
+import { HeroService }     from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 
 @Component({
-    selector: my-app,
-    template:`
-                <h1>{{title}}</h1>
-                <my-heroes></my-heroes>
-             `,
-    directives:[HeroesComponent],
-    providers:[HeroService]
+    selector: 'my-app',
+    template: `
+    <h1>{{title}}</h1>
+    <my-heroes></my-heroes>
+  `,
+    directives: [HeroesComponent, ROUTER_DIRECTIVES],
+    providers: [
+        ROUTER_PROVIDERS,
+        HeroService
+    ]
 })
-export class AppComponent{
-    public title:string = 'Tour of Heroes';
+
+export class AppComponent {
+    title = 'Tour of Heroes';
 }
