856168f65b72f46ec9393ae6fa19e63b13060c1c
kwesiadams <kwesi.adams@gmail.com>
2016-03-29 14:25:28 -0700

    Partial commit

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,12 +1,20 @@
 /**
  * Created by Kwesi on 3/28/16.
  */
+import {HEROES} from './mock-heroes';
 import {Injectable} from 'angular2/core';
+import {Hero} from './hero';
+import set = Reflect.set;
 
 @Injectable()
 export class HeroService{
     getHeroes(){
-        
+        return Promise.resolve(HEROES);
+    }
+    getHeroesSlowly(){
+        return new Promise<Hero[]>(resolve =>
+            setTimeout(()=>resolve(HEROES),2000)
+        );
     }
 
 }
